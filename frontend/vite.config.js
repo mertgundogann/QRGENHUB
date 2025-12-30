54aee0606aad77a741e02dad4fbd78ddc3437a45
Mert Gündoğan <merttgundogan@hotmail.com>
2025-12-30 12:57:34 +0300

    import edited

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from 'vite-plugin-sitemap';
-import { blogPosts } from './blogPosts'; // Blog verilerini import et
+import { blogPosts } from "../frontend/src/data/posts.js"
 
 export default defineConfig(({ mode }) => {
   // .env dosyasını yükle
